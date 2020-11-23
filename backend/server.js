044bd9cd309878e568fceea40d8cc7f5f280c55a
GiorgiR <25549786+GiorgioRR@users.noreply.github.com>
2020-11-23 21:19:10 +0400

    Added needed elements

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config(); // variables
+
+// express server
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connect;
+connection.once('open', () => {
+    console.log("Succesfully established connection with mongodb connection");
+});
+
+// require models
+const usersRouter = require('./route/users'); // Todo: this section should be finished
+
+app.use('users', usersRouter);
+
+// listen
+app.listen(port, () => {
+    console.log(`Server is fcking listening on port ${port}...`);
+});
