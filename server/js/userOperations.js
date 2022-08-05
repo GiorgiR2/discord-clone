e42ebd12dd06190a37280fffe48e653f6665e7d0
Giorgi Rakviashvili <rakviashvili.giorgi@gmail.com>
2022-08-05 23:40:00 +0400

    fix login bug

--- server/js/userOperations.js
+++ server/js/userOperations.js
@@ -1,28 +1,27 @@
+const UserModel = require("../models/user.model");
 
-const UserModel = require('../models/user.model');
+const catOps = require("../js/catOperations");
+const { saveModel } = require("./saveModel");
 
-const catOps = require('../js/catOperations');
-const { saveModel } = require('./saveModel');
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const checkUser = (username) => {
   let user = UserModel.find({
-    username: username
+    username: username,
   })
-  .then(doc => console.log(doc))
-  .catch(err => console.error(err));
+    .then((doc) => console.log(doc))
+    .catch((err) => console.error(err));
 
   console.log(user);
-  return {}
-}
+  return {};
+};
 
 const registerUser = async (username, password0, password1, res) => {
   const doc = await UserModel.find({
-    username: username
+    username: username,
   });
 
-  if (password0 === password1 && doc.length === 0){
+  if (password0 === password1 && doc.length === 0) {
     let user = new UserModel({
       _id: new mongoose.Types.ObjectId(),
       username: username,
@@ -33,45 +32,46 @@ const registerUser = async (username, password0, password1, res) => {
     saveModel(user);
 
     return "done";
-  }
-  else
-    return "try again";
-}
+  } else return "try again";
+};
 
 const addIp = async (username, ip) => {
-  await UserModel.updateOne({
-    username: username,
-  },
-  {
-    $set: {
-      ip: ip,
+  await UserModel.updateOne(
+    {
+      username: username,
+    },
+    {
+      $set: {
+        ip: ip,
+      },
     }
-  })
-  .exec()
-  .then(_ => console.log("ip added..."))
-  .catch(err => console.log(err));
-}
+  )
+    .exec()
+    .then((_) => console.log("ip added..."))
+    .catch((err) => console.log(err));
+};
 
 const removeIp = async (ip) => {
-  await UserModel.updateOne({
-    ip: ip,
-  },
-  {
-    $set: {
-      ip: "",
+  await UserModel.updateOne(
+    {
+      ip: ip,
+    },
+    {
+      $set: {
+        ip: "",
+      },
     }
-  });
+  );
   return "done";
-}
+};
 
 const checkIp = async (ip) => {
   console.log("checking ip...", ip);
   const doc = await UserModel.find({
-    ip: ip
-  })
-  .exec();
+    ip: ip,
+  }).exec();
 
-  if (doc.length !== 0){
+  if (doc.length !== 0) {
     let cats = await catOps.loadCats();
 
     return {
@@ -80,44 +80,50 @@ const checkIp = async (ip) => {
       categories: cats,
       authentication: doc[0]._id,
     };
-  }
-  else
-    return { status: "0" };
-}
+  } else return { status: "0" };
+};
 
 const checkLogin = async (username, password) => {
   let doc = await UserModel.find({
-    username: username
-  })
-  .exec();
+    username: username,
+  }).exec();
 
-  let pswrd = String(doc[0].password);
-  console.log(pswrd === password);
-  if (password === pswrd) // return doc[0].hash;
-    return "done";
-  else
-    return "try again";
-}
+  if (doc[0] == undefined) return "try again";
+  else {
+    let pswrd = String(doc[0].password);
+    console.log(pswrd === password);
+    if (password === pswrd)
+      // return doc[0].hash;
+      return "done";
+    else return "try again";
+  }
+};
 
 const usersStatus = async () => {
   let users = [];
 
   await UserModel.find()
-  .exec()
-  .then(doc => {
-    doc.forEach(el => {
-      users.push({
-        username: el.username,
-        status: "offline",
-        tabsOpen: 0,
+    .exec()
+    .then((doc) => {
+      doc.forEach((el) => {
+        users.push({
+          username: el.username,
+          status: "offline",
+          tabsOpen: 0,
+        });
       });
     });
-  });
 
   return users;
-}
+};
 
-module.exports = { checkUser, registerUser, addIp,
-                   checkLogin, removeIp, checkIp,
-                   usersStatus,
-                  };
+module.exports = {
+  checkUser,
+  registerUser,
+  addIp,
+  checkLogin,
+  removeIp,
+  checkIp,
+  usersStatus,
+};
+
