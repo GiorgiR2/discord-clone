2ce869f60b4c29fbcb2221eb6990e93b1c7a048f
Giorgi Rakviashvili <rakviashvili.giorgi@gmail.com>
2023-02-18 22:46:34 +0400

    minimal ui update

--- client/src/components/Chat/popup/_settings.tsx
+++ client/src/components/Chat/popup/_settings.tsx
@@ -16,6 +16,8 @@ import InputComponent from "../../Login&Signup/inputComponent/inputComponent";
 import "./_popup.sass";
 import "./_settings.sass";
 
+const egressSVG: string = require("../../../assets/egress.svg").default;
+
 const apiLink = packageJson.proxy;
 
 const PopupSettings = () => {
@@ -85,7 +87,15 @@ const PopupSettings = () => {
     return (
         <div className="popup settings">
             <div className="center">
-                <h4 className="mainTitle">PROFILE SETTINGS_</h4>
+                <div className="main">
+                    <h4 className="mainTitle">PROFILE SETTINGS_</h4>
+                    <img
+                    className="x"
+                    src={egressSVG}
+                    alt="exit"
+                    onClick={() => dispatch(toggleSettings())}
+                    />
+                </div>
 
                 <div className="changePassword">
                     <h5>Change Password</h5>
@@ -108,7 +118,7 @@ const PopupSettings = () => {
                 {/* @ts-ignore */}
                 <h5 className="modify" onClick={() => changePassword()}>save</h5>
                 <h5 className="delete" onClick={() => deleteAccount()}>Delete Account</h5>
-                <h5 className="cancel" onClick={() => dispatch(toggleSettings())}>CLOSE</h5>
+                {/* <h5 className="cancel" onClick={() => dispatch(toggleSettings())}>CLOSE</h5> */}
             </div>
         </div>
     );
